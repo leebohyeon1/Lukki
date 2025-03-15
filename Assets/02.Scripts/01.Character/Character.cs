fec1eed161463601e97899bf20d441a524e9f1e5
SeonggyuL <seongk20198@gmail.com>
2025-03-15 13:46:23 +0900

    .

---
+++ Assets/02.Scripts/01.Character/Character.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 모든 움직이는 생명체의 부모 오브젝트 ( 플레이어, 적 등등 )
+/// </summary>
+public class Character : MonoBehaviour
+{
+    protected int _maxHp;           // 최대 체력
+    private int _curHp;             // 현재 체력
+
+    protected float _moveSpeed;     // 움직이는 속도
+
+    protected int _attackDamage;    // 캐릭터의 기본 공격 공격력
+
+
+    protected void Start()
+    {
+        Initialize();
+    }
+
+    /// <summary>
+    /// 클래스를 초기화하는 함수로 
+    /// 처음 시작 시에만 호출된다.
+    /// </summary>
+    protected void Initialize()
+    {
+        _curHp = _maxHp;
+    }
+
+    /// <summary>
+    /// 공격을 받았을 때 호출되는 함수이고 
+    /// 캐릭터의 체력을 감소한다.
+    /// </summary>
+    /// <param name="damage">받는 데미지</param>
+    public void TakeDamage(int damage)
+    {
+        _curHp -= damage;
+
+        if (_curHp <= 0)
+        {
+            Die();
+        }
+    }
+
+    /// <summary>
+    /// 플레이어의 체력이 0보다 낮아지면 호출되면
+    /// </summary>
+    protected void Die()
+    {
+        Debug.Log($"{gameObject.name}- 죽음");
+    }
+}

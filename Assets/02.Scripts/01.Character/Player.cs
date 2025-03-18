aa812ef2cd3d3cae24ff8945f4934bdc8004f7fd
SeonggyuL <seongk20198@gmail.com>
2025-03-18 19:10:52 +0900

    Player.cs Update

--- Assets/02.Scripts/01.Character/Player.cs
+++ Assets/02.Scripts/01.Character/Player.cs
@@ -1,15 +1,32 @@
 using Sirenix.OdinInspector;
 using System;
+using UnityEngine;
 
 public class Player : Character
 {
     public static event Action OnDeath;
 
+    [FoldoutGroup("참조")]
+    [SerializeField] private InputManager _inputManager;
+    [FoldoutGroup("참조")]
+    [SerializeField] private VehicleController _vehicleController;
+
+    [FoldoutGroup("플레이어 상태"), ReadOnly]
+    [SerializeField] private Vector2 _moveInput;
+    [FoldoutGroup("플레이어 상태"), ReadOnly]
+    [SerializeField] private bool _isBraking;
+
+
+    // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (_vehicleController == null || _inputManager == null) return;
 
+        _moveInput = _inputManager.MoveInput;
+        _isBraking = _inputManager.IsBraking;
+        _vehicleController.SetInput(_inputManager.MoveInput, _inputManager.IsBraking);
+
+    }
     [Button]
     protected override void Die()
     {

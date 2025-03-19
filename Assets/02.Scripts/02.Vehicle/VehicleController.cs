d8de629452b517290490deaf3ca3c4d9d758244b
SeonggyuL <seongk20198@gmail.com>
2025-03-20 03:38:39 +0900

    vehicle edit
    평지에서는 정상작동한다 하지만 경사면
    terrain의 굴곡등 여러 지형에서 조향각이 멋대로 바뀌는 현상 있음
    이를 해결하면 차량 움직임은 종료

--- Assets/02.Scripts/02.Vehicle/VehicleController.cs
+++ Assets/02.Scripts/02.Vehicle/VehicleController.cs
@@ -15,15 +15,19 @@ public class VehicleController : MonoBehaviour
     [SerializeField] private Vector2 _moveInput;
     [FoldoutGroup("차량 스체이터스"), ReadOnly]
     [SerializeField] private bool _isBraking;
+    private Rigidbody _rb;
 
     private float _smoothSteering = 0f;
 
     private void Start()
     {
+        _rb = GetComponent<Rigidbody>();
         if(!_engine || !_wheel)
         {
             Debug.LogError("Engine 또는 Wheel 컴포넌트 가 없음");
         }
+
+        _rb.centerOfMass = new Vector3(0, -2.0f, 0); // 차량의 안정성을 위해서 무게중심을 낮춤 필요시  Y값 변경
     }
 
     public void SetInput(Vector2 input, bool brake)
@@ -55,6 +59,41 @@ public class VehicleController : MonoBehaviour
         {
             _wheel.ResetSteering();
         }
+
+        StablizeSteering();
+
+        if(Mathf.Abs(steering)>0.1f)
+        {
+            _wheel.Steer(_smoothSteering);
+        }
+        else
+        {
+            _wheel.ResetSteering();
+        }
         
+    }
+
+    // 지면의 경사에 따른 조향각 안정화
+    public void StablizeSteering()
+    {
+        RaycastHit hitFront, hitRear; // 앞 뒤로 레이를 두개를 사용하여 평균 기울기를 계산하여 안정화에 사용
+
+        Vector3 frontRayPos = transform.position + transform.forward * 1.5f;
+        Vector3 rearRayPos = transform.position - transform.forward * 1.5f;
+
+        bool hasFrontHit = Physics.Raycast(frontRayPos, Vector3.down, out hitFront, 2.0f);
+        bool hasRearHit = Physics.Raycast(rearRayPos, Vector3.down, out hitRear, 2.0f);
+
+        if(hasFrontHit && hasRearHit)
+        {
+            float slopeAngleFront = Vector3.Angle(hitFront.normal, Vector3.up);
+            float slopeAngleRear= Vector3.Angle(hitRear.normal, Vector3.up);
+            float avgSlopeAngle = (slopeAngleFront + slopeAngleRear) * 0.5f;
+
+            if(avgSlopeAngle >10f)
+            {
+                _smoothSteering *= 0.7f;
+            }
+        }
     }
 }
